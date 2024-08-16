ee0c3cdde91e7308e2f3680653401daa5f038227
Scott Ellis <scottellis94@gmail.com>
2024-08-15 23:00:06 -0400

    initial commit

---
+++ pico_parts/Button.py
@@ -0,0 +1,40 @@
+from machine import Timer, Pin, enable_irq, disable_irq
+class Button:
+
+    def __repr__(self):
+        return f"Button({self.button}, {self.button.value() == 0})"
+
+    def __init__(self, button_pin: int, callback=lambda: print("No callback set!")):
+        self.button = Pin(button_pin, Pin.IN)
+        self.callback = callback
+        self.button.irq(trigger=Pin.IRQ_FALLING, handler=self.__button_pressed)
+        self.button_bounces = 0
+        self.cooling_down = False
+        self.timer = Timer()
+
+    def __button_pressed(self, *args):
+        irq_state = disable_irq()
+        if self.cooling_down:
+            enable_irq(irq_state)
+            return
+        enable_irq(irq_state)
+        self.__debounce()
+        print("Button pressed!")
+        self.callback()
+
+    def is_pressed(self, *args):
+        return self.button.value() == 0
+
+    def is_released(self, *args):
+        return self.button.value() == 1
+
+    def __reset_cooldown(self, *args):
+        self.cooling_down = False
+
+    def __debounce(self):
+        print("Debouncing!")
+        self.cooling_down = True
+        self.timer.init(mode=Timer.ONE_SHOT, period=350, callback=self.__reset_cooldown)
+
+    def set_callback(self, callback):
+        self.callback = callback
---
+++ pico_parts/LED.py
@@ -0,0 +1,19 @@
+from machine import Pin
+
+
+class LED:
+
+    def __repr__(self):
+        return f"LED({self.led}, {self.led.value() == 1})"
+
+    def __init__(self, led_pin: int):
+        self.led = Pin(led_pin, Pin.OUT)
+
+    def on(self, *args):
+        self.led.on()
+
+    def off(self, *args):
+        self.led.off()
+
+    def toggle(self, *args):
+        self.led.value(not self.led.value())
